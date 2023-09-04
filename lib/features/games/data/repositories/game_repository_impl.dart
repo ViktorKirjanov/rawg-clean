177398210c6524a9356f6929bf645b177573a8ce
Viktor Kirjanov <viktor.kirjanov@gmail.com>
2023-09-04 20:16:59 +0200

    feat: add GameRepository local database methods

--- lib/features/games/data/repositories/game_repository_impl.dart
+++ lib/features/games/data/repositories/game_repository_impl.dart
@@ -4,15 +4,21 @@ import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:rawg_clean/core/constants/constants.dart';
 import 'package:rawg_clean/core/resources/data_state.dart';
+import 'package:rawg_clean/features/games/data/datasources/local/app_database.dart';
 import 'package:rawg_clean/features/games/data/datasources/remote/games_api_service.dart';
 import 'package:rawg_clean/features/games/data/models/game_model.dart';
 import 'package:rawg_clean/features/games/data/models/pagination_model.dart';
+import 'package:rawg_clean/features/games/domain/entities/game_entity.dart';
 import 'package:rawg_clean/features/games/domain/repositories/game_repository.dart';
 
 class GameRepositoryImpl extends GameRepository {
-  GameRepositoryImpl(this._gamesDataSource);
+  GameRepositoryImpl(
+    this._gamesDataSource,
+    this._appDatabase,
+  );
 
   final GamesDataSource _gamesDataSource;
+  final AppDatabase _appDatabase;
 
   @override
   Future<DataState<PaginationModel<GameModel>>> getGames() async {
@@ -40,4 +46,13 @@ class GameRepositoryImpl extends GameRepository {
       return DataFailed(error);
     }
   }
+
+  @override
+  Future<List<GameModel>> getSavedGames() async => _appDatabase.gameeDao.getGames();
+
+  @override
+  Future<void> removeGame(GameEntity game) async => _appDatabase.gameeDao.deleteGame(GameModel.fromEntity(game));
+
+  @override
+  Future<void> saveGame(GameEntity game) async => _appDatabase.gameeDao.insertGame(GameModel.fromEntity(game));
 }
--- lib/features/games/domain/repositories/game_repository.dart
+++ lib/features/games/domain/repositories/game_repository.dart
@@ -3,5 +3,13 @@ import 'package:rawg_clean/features/games/domain/entities/game_entity.dart';
 import 'package:rawg_clean/features/games/domain/entities/pagination_entity.dart';
 
 abstract class GameRepository {
+  // API methods
   Future<DataState<PaginationEntity<GameEntity>>> getGames();
+
+  //Local Database methods
+  Future<void> saveGame(GameEntity game);
+
+  Future<void> removeGame(GameEntity game);
+
+  Future<List<GameEntity>> getSavedGames();
 }
