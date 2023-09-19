0e5f177782fd177c75aa3652905973c7e2acc91f
Viktor Kirjanov <viktor.kirjanov@gmail.com>
2023-09-19 22:46:32 +0200

    feat: add FloatingActionButton

--- lib/features/games/presentation/widgets/custom_scroll_view_wrapper.dart
+++ lib/features/games/presentation/widgets/custom_scroll_view_wrapper.dart
@@ -23,11 +23,30 @@ class _CustomScrollViewWrapperState extends State<CustomScrollViewWrapper> {
   final ScrollController _scrollController = ScrollController();
 
   Color _appBarBackgroundColor = AppTheme.darkBlue.withOpacity(.0);
-
+  double opacity = 0.0;
   @override
   void initState() {
     super.initState();
+
     _scrollController.addListener(() {
+      // FloatingActionButton opacity
+      if (_scrollController.offset >= 100) {
+        setState(() {
+          opacity = (_scrollController.offset - 100) / 100;
+        });
+      } else {
+        setState(() {
+          opacity = 0.0;
+        });
+      }
+
+      if (opacity > 1.0) {
+        setState(() {
+          opacity = 1.0;
+        });
+      }
+
+      // SliverAppBar background
       if (_scrollController.offset > 0 && _scrollController.offset <= 75) {
         setState(() {
           _appBarBackgroundColor = AppTheme.darkBlue.withOpacity(_scrollController.offset / 90);
@@ -51,25 +70,54 @@ class _CustomScrollViewWrapperState extends State<CustomScrollViewWrapper> {
   }
 
   @override
-  Widget build(BuildContext context) => CustomScrollView(
-        controller: _scrollController,
-        slivers: [
-          SliverAppBar(
-            pinned: widget.pinned,
-            floating: widget.floating,
-            toolbarHeight: 80,
-            elevation: 0.0,
-            scrolledUnderElevation: 0.0,
-            backgroundColor: _appBarBackgroundColor,
-            shape: const RoundedRectangleBorder(
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(16.0),
-                bottomRight: Radius.circular(16.0),
+  Widget build(BuildContext context) => Stack(
+        children: [
+          CustomScrollView(
+            controller: _scrollController,
+            slivers: [
+              SliverAppBar(
+                pinned: widget.pinned,
+                floating: widget.floating,
+                toolbarHeight: 80,
+                elevation: 0.0,
+                scrolledUnderElevation: 0.0,
+                backgroundColor: _appBarBackgroundColor,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(16.0),
+                    bottomRight: Radius.circular(16.0),
+                  ),
+                ),
+                title: widget.appbar,
+              ),
+              ...widget.slivers,
+            ],
+          ),
+          Positioned(
+            bottom: 0.0,
+            right: 24.0,
+            child: SafeArea(
+              child: Opacity(
+                opacity: opacity,
+                child: FloatingActionButton(
+                  backgroundColor: AppTheme.darkBlue.withOpacity(.5),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(32.0),
+                    ),
+                  ),
+                  child: const Icon(Icons.arrow_upward_rounded),
+                  onPressed: () {
+                    _scrollController.animateTo(
+                      0.0,
+                      duration: const Duration(milliseconds: 500),
+                      curve: Curves.easeOut,
+                    );
+                  },
+                ),
               ),
             ),
-            title: widget.appbar,
           ),
-          ...widget.slivers,
         ],
       );
 }
